fd251d7d3d571f2d6591c42f47d9b0802383401f
Eddy <vincent7697@gmail.com>
2020-02-26 10:55:32 +0800

    minor issue

--- app/Http/Controllers/SignController.php
+++ app/Http/Controllers/SignController.php
@@ -9,7 +9,7 @@ class SignController extends Controller
 {
     public function saveSign(Request $request)
     {
-        // $filename = rand(1111111, 9999999);
+        $filename = rand(1111111, 9999999);
         // $data = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $request->imageBase64));
 
         // file_put_contents(storage_path("img/$filename.png"), $data);
@@ -17,6 +17,6 @@ class SignController extends Controller
         $pdf = PDF::loadView('pdf.result', [
             'image' => $request->imageBase64
         ]);
-        return $pdf->download('invoice.pdf');
+        return $pdf->download("result_$filename.pdf");
     }
 }
